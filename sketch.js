1f52ee45d0fb4b0dcd16a3b4e3b265c85af3ec74
Meowmeow666 <71485983+Meowmeow666@users.noreply.github.com>
2020-12-18 15:03:41 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://meowmeow666.github.io/Aifrog/model.json';
 
 
 function preload() {
